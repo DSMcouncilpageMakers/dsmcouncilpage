a220a09958feec15f62df9226de92a796f02423e
DSMcouncilpage Manager <30287270+DSMcouncilpage@users.noreply.github.com>
2017-07-20 20:29:57 +0900

    Delete database.php

--- database.php
+++
@@ -1,7 +0,0 @@
-<?php @$db = new mysqli('localhost', 'root', 'sda1628sda', 'board');
-  if (mysqli_connect_errno()) {
-    echo "<p><h1>Error: Could not be connected to database.</br>
-    Please try again later</h1></p>";
-    exit;
-  }
-?>
